e8928ca0aa0318c2b9f75c896681666755f921bc
akkaponsomjai <asomjai@palo-it.com>
2023-04-02 11:40:34 +0700

    feat(migrate-v2): fix compiled package

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 export { default } from "./BitkubClient";
 export {
   BalancesResponse as BitkubBalancesReturnType,
-  BitkubBidReturnType,
   BitkubEnvironment,
   BitkubErrorCode,
   BitkubHeaderType,
   BitkubOrderType,
-  BitkubSymbolEnum,
   BitkubSymbolInfo,
   SymbolResponse as BitkubSymbolReturnType,
 } from "./models";
